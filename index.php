63f7143d2d29b20cf258d70b415b12d82678cabc
Aleckenzo <152230830+Aleckenzo@users.noreply.github.com>
2023-12-02 19:10:32 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    if (isset($_POST['submit']))
+    {       
+        include_once('config.php');
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+    
+        $sql = mysqli_query($conn, "INSERT INTO dados (nome, email) VALUES ('$nome', '$email')");
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testando conexão com o Banco</title>
+</head>
+<body>
+    <header>
+        <h1>Preencha os dados aqui</h1>
+    </header>
+    <section>
+        <form action="index.php" method="post">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome">
+
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+
+            <input type="submit" value="Enviar" name="submit" id="submit">
+        </form>
+    </section>
+</body>
+</html>
